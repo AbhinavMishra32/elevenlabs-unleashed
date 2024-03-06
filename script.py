f11835b176a7fcb4bcda6494f338cac736259261
rapidtyper <abhinavmishra3322@gmail.com>
2024-03-06 20:57:43 +0530

    Commented out unused code and adjusted number of account creation threads in UnleashedTTS

--- script.py
+++ script.py
@@ -1,33 +1,32 @@
-from elevenlabs_unleashed.account import create_account
+# from elevenlabs_unleashed.account import create_account
 
-username, password, api_key = create_account()
+# username, password, api_key = create_account()
 
-from elevenlabs_unleashed.manager import ELUAccountManager
-from elevenlabs import generate, set_api_key, play, api
+# from elevenlabs_unleashed.manager import ELUAccountManager
+# from elevenlabs import generate, set_api_key, play, api
 
-eluac = ELUAccountManager(set_api_key, nb_accounts= 2) # Creates a queue of API keys
-eluac.next() # First call will block the thread until keys are generated, and call set_api_key
+# eluac = ELUAccountManager(set_api_key, nb_accounts= 2) # Creates a queue of API keys
+# eluac.next() # First call will block the thread until keys are generated, and call set_api_key
 
-def speak(self, message: str):
-    try:
-        audio = generate(
-            text=message,
-            voice="Josh", # I like this one
-            model="eleven_multilingual_v1"
-        )
-    except elevenlabs.api.error.RateLimitError as e:
-        print("[ElevenLabs] Maximum number of requests reached. Getting a new API key...")
-        eluac.next() # Uses next API key in queue, should be instant as nb_accounts > 1, and will generate a new key in a background thread.
-        speak(message)
-        return
+# def speak(self, message: str):
+#     try:
+#         audio = generate(
+#             text=message,
+#             voice="Josh", # I like this one
+#             model="eleven_multilingual_v1"
+#         )
+#     except elevenlabs.api.error.RateLimitError as e:
+#         print("[ElevenLabs] Maximum number of requests reached. Getting a new API key...")
+#         eluac.next() # Uses next API key in queue, should be instant as nb_accounts > 1, and will generate a new key in a background thread.
+#         speak(message)
+#         return
 
-    print("[ElevenLabs] Starting the stream...")
-    play(audio)
-
+#     print("[ElevenLabs] Starting the stream...")
+#     play(audio)
 
 from elevenlabs_unleashed.tts import UnleashedTTS
 
-tts = UnleashedTTS(nb_accounts=2, create_accounts_threads=2)
+tts = UnleashedTTS(nb_accounts=2, create_accounts_threads=1)
 """
 Will automatically generate 2 accounts in 2 threads. Takes a few seconds.
 """
